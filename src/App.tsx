57ef06218599e7162c3cc98206e97a9e6065b81e
zachpang <zachery.pang@gmail.com>
2024-04-08 02:33:21 +0800

    feat: Add SearchBar component
    - Add heroicons package

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import SearchBar from "./components/SearchBar";
+
 function App() {
   return (
     <>
       <div className="h-screen w-screen bg-gradient-to-tr from-[#4CA1AF] to-[#C4E0E5] to-60%">
-        <main className="container mx-auto"></main>
+        <main className="container mx-auto pt-8">
+          <SearchBar />
+        </main>
       </div>
     </>
   );
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,31 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
+interface SearchBarProps {
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  onSubmit: React.FormEventHandler<HTMLFormElement>;
+  className?: string;
+}
+
+function SearchBar({ value, onChange, onSubmit, className }: SearchBarProps) {
+  return (
+    <search className={`w-full ${className}`}>
+      <form className="flex w-full" onSubmit={onSubmit}>
+        <input
+          className="mr-4 flex-1 rounded-xl border-0 px-4 text-gray-900 opacity-50 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600"
+          type="text"
+          name="search-value"
+          placeholder="Search for a city or country"
+        />
+        <button
+          className="h-[42px] w-[42px] flex-none rounded-xl border-0 bg-[#4CA1AF] transition-transform hover:scale-105 active:scale-95"
+          type="submit"
+        >
+          <MagnifyingGlassIcon className="m-auto h-6 w-6 stroke-white stroke-2" />
+        </button>
+      </form>
+    </search>
+  );
+}
+
+export default SearchBar;
