1c2612c942c9f2d5e2c0332340d9eab56c38427e
zachpang <zachery.pang@gmail.com>
2024-04-07 14:46:27 +0800

    config: Add eslint-plugin-react for react lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
