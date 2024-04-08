b3367249d9fb41cce940f02105d5b3d4b8d588d7
zachpang <zachery.pang@gmail.com>
2024-04-08 12:09:52 +0800

    feat: Add fetch request function for Geocoding API

---
+++ src/api/index.ts
@@ -0,0 +1,64 @@
+/*
+  Geocoding API assumes input is a city, unless a country code is also included.
+
+      {
+          "name": "Singapore",
+          "local_names": {
+              "sk": "Singapur",
+              "ro": "Singapore",
+              "yo": "Singapore",
+              ...
+          },
+          "lat": 1.2899175,
+          "lon": 103.8519072,
+          "country": "SG"
+      }
+
+      {
+        "name": "New York County",
+        "local_names": {
+            "ja": "ニューヨーク",
+            "ca": "Nova York",
+            "fa": "نیویورک",
+            ...
+        },
+        "lat": 40.7127281,
+        "lon": -74.0060152,
+        "country": "US",
+        "state": "New York"
+    },
+*/
+
+export interface CoordinateData {
+  name: string;
+  local_names: Record<string, string>;
+  lat: number;
+  lon: number;
+  country: string;
+  state?: string;
+}
+
+export async function fetchCoordinates(location: string, limit: number) {
+  const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+
+  let response = null;
+  try {
+    response = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=${String(limit)}&appid=${apiKey}`,
+    );
+  } catch (error) {
+    // bad request headers or network error.
+    throw new Error(
+      "Looks like you've lost your internet connection. Try again when you're back online.",
+    );
+  }
+
+  if (!response?.ok) {
+    // Not 2xx. server error
+    throw new Error(
+      "We're experiencing some technical difficulties. Please try again in a few moments.",
+    );
+  }
+
+  return (await response.json()) as CoordinateData[];
+}
